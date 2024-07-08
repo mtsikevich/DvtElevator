ada91eb93279e7f4dcd43c63a2eccb57f6c96a63
Mtsikevich <tumelo.motsikelane@gmail.com>
2024-07-08 21:35:11 +0200

    Add elevator availability custom exception

--- src/DvtElevator/Exceptions/CustomExceptionTypes.cs
+++ src/DvtElevator/Exceptions/CustomExceptionTypes.cs
@@ -2,3 +2,4 @@ namespace DvtElevator.Exceptions;
 
 public class BuildingHasNoElevators() : Exception(string.Empty);
 public class ElevatorExceedsBuildingFloors() : Exception(string.Empty);
+public class ElevatorsNotAvailable(): Exception(string.Empty);
